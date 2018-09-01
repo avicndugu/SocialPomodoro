795b8f899547fff6d6b9e0b25d4b85e4703ae467
avicndugu <avicnduguh@gmail.com>
2018-09-01 12:43:41 +0300

    exit from console added display on page DOM

--- pomoCounter.js
+++ pomoCounter.js
@@ -4,25 +4,37 @@
 // Set up a trigger that will start the counting
 // If the counter reaches zero it sends a trigger that increments the b count by 1
 
-var pomoStartTime=5;
-var shortBreakStartTime= 10;
-var longBreakTimeStartTime= 15;
+var pomoStartTime=10;
+initialMinutes=3;
+var pomoCount=0;
+// var shortBreakStartTime= 10;
+// var longBreakTimeStartTime= 15;
 
 var currentTimeSeconds=pomoStartTime;
+// var showSeconds=document.querySelector('p');
+
+// variables for displaying timer in the page
+var showSeconds=document.getElementById('seconds');
+var showMinutes=document.getElementById('minutes');
+var showPomos=document.getElementById('pomos');
+// Setting up innitial display
+showSeconds.textContent=pomoStartTime;
+showMinutes.textContent=initialMinutes;
+showPomos.textContent=pomoCount;
 
 // countdown in seconds
-setInterval(delaying, 1000);
 function delaying(){
 // checks and counts down
-	 if (currentTimeSeconds>0){
+	 if (currentTimeSeconds>=1){
 		currentTimeSeconds--;
 		// console.log(currentTimeSeconds);
+// displaying timer seconds in the page
+		showSeconds.textContent=currentTimeSeconds;
 	 }
 //Restarts seconds counter after reaching zero
 	 else {
 	 	currentTimeSeconds=pomoStartTime;
 	 	minutesCounter();
-	 	pomoCounter();
 	 }
 }
 
@@ -32,23 +44,28 @@ while (currentTimeSeconds<0) {
 
 // countdown in minutes
 // if currentTime
-initialMinutes=25;
 currentMinutes=initialMinutes;
 // console.log(currentMinutes);
 function minutesCounter(){
 	if (currentMinutes>0){
 		currentMinutes--;
 		// console.log(currentMinutes);
+		showMinutes.textContent=currentMinutes;
+	}
+	else{
+		pomoCounter();
 	}
 }
 
 
 // Pomo's counter; counts the number of pomodoro covered 
-var pomoCount=0;
 function pomoCounter () {
 	pomoCount++;
 	console.log(pomoCount);
+	showPomos.textContent=pomoCount;
 }
+
+setInterval(delaying, 1000);
 
 
 // Need to have a counter for the pomodoro time and a counter for the break time
