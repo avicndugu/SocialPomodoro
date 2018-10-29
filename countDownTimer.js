fb09b4e9d703d0aa29a555c6b5e153bcba868b1b
avicndugu <avicnduguh@gmail.com>
2018-10-29 18:19:35 +0300

    Added additional buttons

--- countDownTimer.js
+++ countDownTimer.js
@@ -3,48 +3,62 @@
 //  1. change the start button in timerButtons.js from stop to start
 //  2. send a trigger to pomoCounter.js to increment by 1
 
-var pomoStartMinutes=2;
-var currentTimeMinutes=pomoStartMinutes;
-var pomoStartSeconds=5;
-var currentTimeSeconds=pomoStartSeconds;
+// var pomoStartMinutes=2;
+// var currentTimeMinutes=pomoStartMinutes;
+// var pomoStartSeconds=5;
+// var currentTimeSeconds=pomoStartSeconds;
 
-// variables for displaying timer in the page
-var showSeconds=document.getElementById('seconds');
-var showMinutes=document.getElementById('minutes');
-var showPomos=document.getElementById('pomos');
-// Setting up innitial display
-showSeconds.textContent=pomoStartSeconds;
-showMinutes.textContent=pomoStartMinutes;
-// showPomos.textContent=pomoCount;
-
-
-//////////// MINUTES COUNTER /////////////////////////
-function minutes() {
-	function delaying(){
-	// checks and counts down
-		 if (currentTimeSeconds>=0){
-			// displaying timer seconds in the page
-			showSeconds.textContent=currentTimeSeconds;
-		 	console.log(currentTimeSeconds);
-			currentTimeSeconds--;
+// // variables for displaying timer in the page
+// var showSeconds=document.getElementById('seconds');
+// var showMinutes=document.getElementById('minutes');
+// var showPomos=document.getElementById('pomos');
+// // Setting up innitial display
+// showSeconds.textContent=pomoStartSeconds;
+// showMinutes.textContent=pomoStartMinutes;
+// // showPomos.textContent=pomoCount;
 
 
-		}
-		else {
-			// console.log(currentTimeMinutes);
-			showMinutes.textContent=currentTimeMinutes;
-			currentTimeMinutes--;
-			currentTimeSeconds=5;
-			if (currentTimeMinutes<0) {
-				currentTimeMinutes=0;
-				currentTimeSeconds=0;
-				stopCounter();
-			}
-		}
-	}
-	secInterval=setInterval(delaying, 1000);
-}
-function stopCounter() {
-	clearInterval(secInterval);
-}
-minutes();
+// //////////// MINUTES COUNTER /////////////////////////
+// function minutes() {
+// 	function delaying(){
+// 	// checks and counts down
+// 		 if (currentTimeSeconds>=0){
+// 			// displaying timer seconds in the page
+// 			showSeconds.textContent=currentTimeSeconds;
+// 		 	console.log(currentTimeSeconds);
+// 			currentTimeSeconds--;
+
+
+// 		}
+// 		else {
+// 			// console.log(currentTimeMinutes);
+// 			showMinutes.textContent=currentTimeMinutes;
+// 			currentTimeMinutes--;
+// 			currentTimeSeconds=5;
+// 			if (currentTimeMinutes<0) {
+// 				currentTimeMinutes=0;
+// 				currentTimeSeconds=0;
+// 				stopCounter();
+// 			}
+// 		}
+// 	}
+// 	secInterval=setInterval(delaying, 1000);
+// }
+// function stopCounter() {
+// 	clearInterval(secInterval);
+// }
+// minutes();
+
+const timer = document.querySelector("#timer");
+let minutes, seconds;
+
+minutes = 10;
+seconds = 20;
+
+// timer.childNodes[1].innerHTML = `${minutes}` + " : " + `${seconds}`;
+
+
+const shower= document.querySelector("#shower");
+minutes = 10;
+seconds = 20;
+shower.innerHTML = minutes + " : " + seconds;
