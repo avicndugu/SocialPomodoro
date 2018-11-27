02a3eeee8cb0117084bee6537d01c5f624c8689f
avicndugu <avicnduguh@gmail.com>
2018-11-27 14:43:26 +0300

    Removed start,pause and reset buttons from view with no error

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ paused.style.display="none";
 reseted.style.display="none";
 shortButton.disabled=true;
 longButton.disabled=true;
-pause.disabled=true;
-reset.disabled=true;
+// pause.disabled=true;
+// reset.disabled=true;
 reseted.style.display="none";
 showDisabled();
 // showMinutes.textContent=pomo.startMinutes;
@@ -52,13 +52,13 @@ var startPomoTimer= function(){
 	// showSeconds.textContent=pomo.startSeconds;
 	pomoSecInterval=setInterval(pomoDelaying, 1000);
 	pomodoro.disabled=true;
-	reset.disabled=false;
+	// reset.disabled=false;
 	reseted.style.display="inline-block";
 	shortButton.disabled=true;
-	pause.disabled=false;
+	// pause.disabled=false;
 	paused.style.display="inline-block";
 	longButton.disabled=true;
-	play.disabled=true;
+	// play.disabled=true;
 	played.style.display="none";
 	showDisabled();
 }
@@ -95,12 +95,12 @@ var pomoDelaying = function (){
 		// Reactivate all the buttons
 		playAudio();
 		pomodoro.disabled=true;
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
 		shortButton.disabled=false;
-		reset.disabled=true;
+		// reset.disabled=true;
 		reseted.style.display="none";
 		// showDisabled();
 		pomoFlow();
@@ -148,11 +148,11 @@ var startShortBTimer= function(){
 	shortButton.disabled=true;
 	longButton.disabled=true;
 	pomodoro.disabled=true;
-	reset.disabled=false;
+	// reset.disabled=false;
 	reseted.style.display="inline-block";
-	pause.disabled=false;
+	// pause.disabled=false;
 	paused.style.display="inline-block";
-	play.disabled=true;
+	// play.disabled=true;
 	played.style.display="none";
 	showDisabled();
 }
@@ -193,9 +193,9 @@ function endOfShortB() {
 	// Reactivate all the buttons
 	playAudio();
 	pomodoro.disabled=false;
-	play.disabled=true;
+	// play.disabled=true;
 	played.style.display="none";
-	pause.disabled=false;
+	// pause.disabled=false;
 	paused.style.display="inline-block";
 	shortButton.disabled=true;
 	longButton.disabled=true;
@@ -215,11 +215,11 @@ var startLongBTimer =function () {
 	longButton.disabled=true;
 	shortButton.disabled=true;
 	pomodoro.disabled=true;
-	reset.disabled=false;
+	// reset.disabled=false;
 	reseted.style.display="inline-block";
-	pause.disabled=false;
+	// pause.disabled=false;
 	paused.style.display="inline-block";
-	play.disabled=true;
+	// play.disabled=true;
 	played.style.display="none";
 	showDisabled();
 }
@@ -258,13 +258,13 @@ function endOfLongB() {
 	// long.disabled=false;
 	playAudio();
 	pomodoro.disabled=false;
-	play.disabled=true;
+	// play.disabled=true;
 	played.style.display="none";
-	pause.disabled=false;
+	// pause.disabled=false;
 	paused.style.display="inline-block";
 	shortButton.disabled=true;
 	longButton.disabled=true;
-	reset.disabled=false;
+	// reset.disabled=false;
 	reseted.style.display="inline-block";
 	showDisabled();
 
@@ -275,27 +275,27 @@ var pausePomoTimer = function (){
 	if (timer==1) {
 		window.clearInterval(pomoSecInterval);
 		pomodoro.disabled=true;
-		play.disabled=false;
+		// play.disabled=false;
 		played.style.display="inline-block";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
 		showDisabled();
 	}
 	if (timer==2) {
 		window.clearInterval(shortSecInterval);
 		shortButton.disabled=true;
-		play.disabled=false;
+		// play.disabled=false;
 		played.style.display="inline-block";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
 		showDisabled();
 	}
 	if (timer==3) {
 		window.clearInterval(longSecInterval);
 		longButton.disabled=true;
-		play.disabled=false;
+		// play.disabled=false;
 		played.style.display="inline-block";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
 		showDisabled();
 	}
@@ -304,33 +304,33 @@ var playPomoTimer = function (){
 	if (timer==1){
 		pomoSecInterval=setInterval(pomoDelaying, 1000);
 		pomodoro.disabled=true;
-		reset.disabled=false;
+		// reset.disabled=false;
 		reseted.style.display="inline-block";
-		pause.disabled=false;
+		// pause.disabled=false;
 		paused.style.display="inline-block";
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
 		showDisabled();
 	}
 	if (timer==2) {
 		shortSecInterval=setInterval(shortDelaying, 1000);
 		shortB.disabled=true;
-		reset.disabled=false;
+		// reset.disabled=false;
 		reseted.style.display="inline-block";
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
-		pause.disabled=false;
+		// pause.disabled=false;
 		paused.style.display="inline-block";
 		showDisabled();
 	}
 	if (timer==3) {
 		longSecInterval=setInterval(longDelaying, 1000);
 		longButton.disabled=true;
-		reset.disabled=false;
+		// reset.disabled=false;
 		reseted.style.display="inline-block";
-		pause.disabled=false;
+		// pause.disabled=false;
 		paused.style.display="inline-block";
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
 		showDisabled();
 	}
@@ -345,11 +345,11 @@ var ResetPomoTimer = function (){
 		// pomo.currentSeconds=pomo.currentSeconds;
 		pomodoro.disabled=false;
 		shortButton.disabled=false;
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
-		reset.disabled=true;
+		// reset.disabled=true;
 		reseted.style.display="none";
 		showDisabled();
 	}
@@ -362,11 +362,11 @@ var ResetPomoTimer = function (){
 		shortButton.disabled=false;
 		pomodoro.disabled=false;
 		longButton.disabled=false;
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
-		reset.disabled=true;
+		// reset.disabled=true;
 		reseted.style.display="none";
 		showDisabled();
 	}
@@ -379,11 +379,11 @@ var ResetPomoTimer = function (){
 		shortButton.disabled=false;
 		pomodoro.disabled=false;
 		longButton.disabled=false;
-		play.disabled=true;
+		// play.disabled=true;
 		played.style.display="none";
-		pause.disabled=true;
+		// pause.disabled=true;
 		paused.style.display="none";
-		reset.disabled=true;
+		// reset.disabled=true;
 		reseted.style.display="none";
 		showDisabled();
 }
